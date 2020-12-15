7722e29aac8701f3734ef169c7ce6041821c0cb0
Anusha Kataria <anushak@nseit.com>
2020-12-15 17:15:13 +0530

    initial files

---
+++ server/server/__init__.py
@@ -0,0 +1,14 @@
+from pyramid.config import Configurator
+
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    with Configurator(settings=settings) as config:
+        config.include('.models')
+        config.include('pyramid_jinja2')
+        config.include('.routes')
+        config.add_route('home', '/')
+        config.add_static_view('/', settings.get('client_url'))
+        config.scan()
+    return config.make_wsgi_app()
