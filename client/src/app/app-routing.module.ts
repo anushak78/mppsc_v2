7c126789d54f3932f2887a87b9f36881ed2aadc1
niraj <nmandaliya@nseit.com>
2020-12-24 11:16:16 +0530

    user-master

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -16,11 +16,11 @@ const routes: Routes = [
     path: 'login',
     component: LoginComponent
   },
-  {
-    path: 'home',
-    redirectTo: 'login', pathMatch: 'full',
-    component: LoginComponent
-  },
+  // {
+  //   path: 'home',
+  //   redirectTo: 'login', pathMatch: 'full',
+  //   component: LoginComponent
+  // },
   {
     path: '',
     component: LayoutComponent,
