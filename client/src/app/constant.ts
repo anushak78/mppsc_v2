93e533fdef625ec30afadd67e7e651c78a7458e8
Krishna Mishra <krishna.meashra@gmail.com>
2020-12-30 10:45:31 +0530

    Client: added user master active status and designation.

---
+++ client/src/app/constant.ts
@@ -0,0 +1,15 @@
+export const Roles = {
+  1: 'Board Member',
+  2: 'Chairman',
+  3: 'Verification Officer'
+};
+export const Titles = {
+  1: 'Mr.',
+  2: 'Mrs.',
+  3: 'Miss',
+  4: 'Dr.',
+};
+export const Status = {
+  1: 'Active',
+  0: 'Inactive'
+};
